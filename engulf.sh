3f22f01c7c41470706b24b1a328f6f596bb15fff
shisui98 <mitkar@tutanota.com>
2023-05-20 20:41:24 +0530

    Tweaked order to fix bug

--- engulf.sh
+++ engulf.sh
@@ -2,17 +2,20 @@
 
 WID=$(xdo id)
 
-if [ $# -ne 2 ]; then
-    echo "Two options required"
-    exit 1
-elif [ $# -eq 2 ]; then
+if [ $# -eq 0 ]; then
+    echo "Need more arguments to work!"
+elif [ $# -eq 1 ]; then
     xdo hide $WID
-    bash -c "$1 $2 > /dev/null 2>&1; exit"
+    bash -i -c "$1 > /dev/null 2>&1; exit 0"
     xdo show $WID
 elif [ $2 = "fzf" ]; then
-    file=$(printf '%q' "$(fzf)")
+    file=$(printf '%q' "`fzf`")
     xdo hide $WID
-    bash -c "$1 $file > /dev/null 2>&1; exit"
+    bash -i -c "$1 $file > /dev/null 2>&1; exit 0"
+    xdo show $WID
+elif [ $# -eq 2 ]; then
+    xdo hide $WID
+    bash -i -c "$1 $2 > /dev/null 2>&1; exit 0"
     xdo show $WID
 else
     exit 2

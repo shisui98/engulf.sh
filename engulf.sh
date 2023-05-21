409a2e24500f16fc60f34a766b2d1ea5e315f25d
shisui98 <mitkar@tutanota.com>
2023-05-21 15:16:00 +0530

    Fixed bug and added ranger support

--- engulf.sh
+++ engulf.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-
+FZF="fzf"
 WID=$(xdo id)
 
 if [ $# -eq 0 ]; then
@@ -8,14 +8,15 @@ elif [ $# -eq 1 ]; then
     xdo hide $WID
     bash -i -c "$1 > /dev/null 2>&1; exit 0"
     xdo show $WID
-elif [ $2 = "fzf" ]; then
+elif [ "$2" = "$FZF" ]; then
     file=$(printf '%q' "`fzf`")
     xdo hide $WID
     bash -i -c "$1 $file > /dev/null 2>&1; exit 0"
     xdo show $WID
 elif [ $# -eq 2 ]; then
+    file2=$(printf '%q' "$2")
     xdo hide $WID
-    bash -i -c "$1 $2 > /dev/null 2>&1; exit 0"
+    bash -i -c "$1 $file2 > /dev/null 2>&1; exit 0"
     xdo show $WID
 else
     exit 2
